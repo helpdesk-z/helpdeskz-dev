dfed981ac8bcfcab65531ae985d2be35feca5152
andres <ajms1008@gmail.com>
2021-01-14 16:56:16 -0500

    Update of variables

--- hdz/install/upgrade_evo.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `{{db_prefix}}config` ADD `id` INT(11) NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (`id`);
-ALTER TABLE `{{db_prefix}}config` CHANGE `logo` `logo` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
