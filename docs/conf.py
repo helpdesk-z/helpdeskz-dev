4a7ab7ae069427d49cd1fdb6565e3014e42f961c
andres <ajms1008@gmail.com>
2021-01-25 20:10:56 -0500

    API documentation

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,6 @@ extensions = [
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
-    'sphinxcontrib.httpdomain',
     'djangodocs',
     'doc_extensions',
     'sphinx_tabs.tabs',
